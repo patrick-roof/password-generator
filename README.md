659051ab97ec2f2b081ab1eeef1e660e16877aaf
patrick <roosterruf@gmail.com>
2022-11-09 19:37:09 -0700

    final commit, changed README and js

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Description
 
+
+
 This project will create a random password using criteria selected by the user. It will iclude a series of window prompts asking how long the password should be, and which criteria to include. The generated password will be displayed in the box to be copied.
 
 ## What I've learned
 
-During this project I've learned a significant amount about JavaScript, for loops, and functions.
-# password-generator
-# password-generator
+During this project I've learned a significant amount about JavaScript, for-loops, and functions.
+
+
+![password generator screenshot](https://user-images.githubusercontent.com/115768554/200978663-a048be80-bbb2-48bd-adc3-2f74e4da5b0d.png)
--- script.js
+++ script.js
@@ -8,13 +8,13 @@ var pwChoices = [];
 
 var finalPass = [];
 
-var lowercaseArray = ['a','b','c','d','e','f','g'];
+var lowercaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-var uppercaseArray = ['A','B','C','D','E','F','G'];
+var uppercaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-var numbersArray = ['1','2','3','4','5','6','7'];
+var numbersArray = ['1','2','3','4','5','6','7','8','9','0'];
 
-var symbolsArray = ['!','@','#','$','%','^','&'];
+var symbolsArray = ['!','@','#','$','%','^','&','*','(',')','{','}','<','>'];
 
 
 function generatePassword() {
